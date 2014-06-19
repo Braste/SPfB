40d1babfc1c7fcd11f390bb60c1c04c073978dc3
Stefan Braun <MMg84gNs>
2014-06-19 12:59:36 +0200

    command filter should now work

--- src/main/java/de/braste/SPfB/functions/CommandFilter.java
+++ src/main/java/de/braste/SPfB/functions/CommandFilter.java
@@ -7,11 +7,6 @@ public class CommandFilter implements Filter {
 
     @Override
     public boolean isLoggable(LogRecord record) {
-        if(record.getMessage().contains("issued server command: /login")) {
-            int endIndex = record.getMessage().indexOf("/login");
-            record.setMessage(record.getMessage().substring(0, endIndex + 6));
-        }
-        record.setMessage(record.getMessage() + "1");
-        return true;
+        return !record.getMessage().toLowerCase().contains("issued server command: /login");
     }
 }
